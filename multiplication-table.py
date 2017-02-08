0aa3898168478c01291768e9cbaa874be7b6b4f3
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-08 13:59:30 -0500

    Updated from Brython Server: February 8, 2017 1:59:31 PM EST

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,3 +21,14 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+width = int(input("Width of multiplication table: "))
+height = int(input("Height of multiplication table: "))
+
+for i in range (1, width):
+    for j in range(i):
+        print (i,j)
+
+
+
+
+
