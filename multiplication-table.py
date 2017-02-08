2ccc0c1ccbf4f2a38f213081e56acfe0ef9cf848
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-08 13:44:50 -0500

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: will laycock
+Credit: done myself
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
